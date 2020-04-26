42dd5eb661260b6407cc5a7a55b8b73043f503a3
Ramon Melo <me@ramonmelo.com>
2020-04-26 16:45:10 -0300

    ADDED: Use of sensors

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,12 @@
 
 //Libraries for LoRa
 #include <SPI.h>
-// #include <LoRa.h>
+#include <LoRa.h>
 
 //Libraries for OLED Display
-#include <Wire.h>
-// #include <Adafruit_GFX.h>
-// #include <Adafruit_SSD1306.h>
+#include <Wire.h> // I2C
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
 // Sensors
 #include <sensors/Sensor.h>
@@ -18,29 +18,29 @@
 #define DHT_PIN 17
 
 //define the pins used by the LoRa transceiver module
-// #define SCK 5
-// #define MISO 19
-// #define MOSI 27
-// #define SS 18
-// #define RST 14
-// #define DIO0 26
+#define SCK 5
+#define MISO 19
+#define MOSI 27
+#define SS 18
+#define RST 14
+#define DIO0 26
 
 //433E6 for Asia
 //866E6 for Europe
 //915E6 for North America
-// #define BAND 915E6
+#define BAND 915E6
 
 //OLED pins
-// #define OLED_SDA 4
-// #define OLED_SCL 15
-// #define OLED_RST 16
-// #define SCREEN_WIDTH 128 // OLED display width, in pixels
-// #define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_SDA 4
+#define OLED_SCL 15
+#define OLED_RST 16
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
 //packet counter
-// int counter = 0;
+int counter = 0;
 
-// Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RST);
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RST);
 
 // Sensors
 
@@ -59,45 +59,45 @@ void setup()
 	sensors.push_back(new InovaBee::SensorDHT(DHT_PIN));
 
 	//reset OLED display via software
-	// pinMode(OLED_RST, OUTPUT);
-	// digitalWrite(OLED_RST, LOW);
-	// delay(20);
-	// digitalWrite(OLED_RST, HIGH);
+	pinMode(OLED_RST, OUTPUT);
+	digitalWrite(OLED_RST, LOW);
+	delay(20);
+	digitalWrite(OLED_RST, HIGH);
 
-	// //initialize OLED
-	// Wire.begin(OLED_SDA, OLED_SCL);
-	// if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3c, false, false))
-	// { // Address 0x3C for 128x32
-	// 	Serial.println(F("SSD1306 allocation failed"));
-	// 	for (;;)
-	// 		; // Don't proceed, loop forever
-	// }
+	//initialize OLED
+	Wire.begin(OLED_SDA, OLED_SCL);
+	if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3c, false, false))
+	{ // Address 0x3C for 128x32
+		Serial.println(F("SSD1306 allocation failed"));
+		for (;;)
+			; // Don't proceed, loop forever
+	}
 
-	// display.clearDisplay();
-	// display.setTextColor(WHITE);
-	// display.setTextSize(1);
-	// display.setCursor(0, 0);
-	// display.print("LORA SENDER ");
-	// display.display();
+	display.clearDisplay();
+	display.setTextColor(WHITE);
+	display.setTextSize(1);
+	display.setCursor(0, 0);
+	display.print("LORA SENDER ");
+	display.display();
 
-	// //initialize Serial Monitor
+	//initialize Serial Monitor
 
-	// //SPI LoRa pins
-	// SPI.begin(SCK, MISO, MOSI, SS);
-	// //setup LoRa transceiver module
-	// LoRa.setPins(SS, RST, DIO0);
+	//SPI LoRa pins
+	SPI.begin(SCK, MISO, MOSI, SS);
+	//setup LoRa transceiver module
+	LoRa.setPins(SS, RST, DIO0);
 
-	// if (!LoRa.begin(BAND))
-	// {
-	// 	Serial.println("Starting LoRa failed!");
-	// 	while (1)
-	// 		;
-	// }
-	// Serial.println("LoRa Initializing OK!");
-	// display.setCursor(0, 10);
-	// display.print("LoRa Initializing OK!");
-	// display.display();
-	// delay(2000);
+	if (!LoRa.begin(BAND))
+	{
+		Serial.println("Starting LoRa failed!");
+		while (1)
+			;
+	}
+	Serial.println("LoRa Initializing OK!");
+	display.setCursor(0, 10);
+	display.print("LoRa Initializing OK!");
+	display.display();
+	delay(2000);
 }
 
 void serviceSensor()
@@ -117,32 +117,30 @@ void loop()
 {
 	serviceSensor();
 
-	delay(1000);
+	Serial.print("Sending packet: ");
+	Serial.println(counter);
 
-	// Serial.print("Sending packet: ");
-	// Serial.println(counter);
+	//Send LoRa packet to receiver
+	if (LoRa.beginPacket())
+	{
+		LoRa.print("hello ");
+		LoRa.print(counter);
+		LoRa.endPacket();
 
-	// //Send LoRa packet to receiver
-	// if (LoRa.beginPacket())
-	// {
-	// 	LoRa.print("hello ");
-	// 	LoRa.print(counter);
-	// 	LoRa.endPacket();
+		display.clearDisplay();
+		display.setCursor(0, 0);
+		display.println("LORA SENDER");
+		display.setCursor(0, 20);
+		display.setTextSize(1);
+		display.print("LoRa packet sent.");
+		display.setCursor(0, 30);
+		display.print("Counter:");
+		display.setCursor(50, 30);
+		display.print(counter);
+		display.display();
 
-	// 	display.clearDisplay();
-	// 	display.setCursor(0, 0);
-	// 	display.println("LORA SENDER");
-	// 	display.setCursor(0, 20);
-	// 	display.setTextSize(1);
-	// 	display.print("LoRa packet sent.");
-	// 	display.setCursor(0, 30);
-	// 	display.print("Counter:");
-	// 	display.setCursor(50, 30);
-	// 	display.print(counter);
-	// 	display.display();
+		counter++;
+	}
 
-	// 	counter++;
-	// }
-
-	// delay(10000);
+	delay(10000);
 }
